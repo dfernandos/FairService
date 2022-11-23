63257de79bae3a29dfa8c1d15a77801747fd735e
Daniel Oliveira <danieldfs18@hotmail.com>
2022-11-23 18:50:46 -0300

    create marker class

---
+++ src/main/java/com/feirapp/fairService/utils/Marker.java
@@ -0,0 +1,17 @@
+package com.feirapp.fairService.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Marker {
+
+    private String name;
+    private double latitude;
+    private double longitude;
+}
